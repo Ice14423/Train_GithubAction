44eae5410436855164715a260b1f7074673728e5
Ice14423 <nrathron1@gmail.com>
2025-12-10 14:06:45 +0700

    add test automate

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const express = require("express");
+function makeApp(database){
+    const app = express();
+    app.use(express.json())
+    app.post("/", async (req,res) => {
+        const {username, password} = req.body;
+        if (!password || !username){
+            res.sendStatus(400)
+            return
+        }
+        const newUser = await database.createUser(username,password);
+        res.status(200).json(newUser)
+    })
+
+    return app
+}
+
+module.exports = makeApp
---
+++ app.test.js
@@ -0,0 +1,20 @@
+const request = require("supertest");
+const makeApp = require("./app.js");
+
+const mockCreateUser = jest.fn();
+const mockDatabase = {};
+mockDatabase.createUser = mockCreateUser;
+const app = makeApp(mockDatabase);
+
+test("should save the username and password to the database", async () => {
+    const bodyData = { username: "username1", password: "password1"}
+    //ให้ mockCreateUser ส่งข้อมูล id usrname และ password กลับไปเมื่อถูกเรียกใช้งาน
+    mockCreateUser.mockResolvedValue({ id: 1, ...bodyData });
+    const response = await request(app).post("/").send(bodyData);
+    expect(mockCreateUser.mock.calls[0][0]).toBe(bodyData.username);
+    expect(mockCreateUser.mock.calls[0][1]).toBe(bodyData.password);
+    expect(response.statusCode).toBe(200); // ตรวจสอบว่า status code = 200
+    expect(response.body.id).toBe(1); // ตรวจสอบว่า id = 1
+    expect(response.body.username).toBe(bodyData.username);
+    expect(response.body.password).toBe(bodyData.password);
+  });
